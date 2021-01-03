8d67957f755e9b431c3903e643b332e517d73ce8
Kina <CrafterKina@users.noreply.github.com>
2021-01-03 19:16:19 +0900

    Striktlify tests to fix bugs

--- src/__tests__/Pagination.tsx
+++ src/__tests__/Pagination.tsx
@@ -55,11 +55,26 @@ describe("Pagination機能はArray要素を移動する機能を持つ", () => {
         // 初期化コール
         expect(handlePageChange).toHaveBeenLastCalledWith(2);
 
-        rerender(<Pagination paginateObjects={[0, 1]} handlePageChange={handlePageChange}/>)
+        rerender(<Pagination paginateObjects={[0]} handlePageChange={handlePageChange}/>)
 
         // Propの変更によるコール
         expect(handlePageChange).not.toHaveBeenLastCalledWith(undefined); // undefinedには決してならない
-        expect(handlePageChange).toHaveBeenLastCalledWith(1); // 現在の終端
+        expect(handlePageChange).toHaveBeenLastCalledWith(0); // 現在の終端
+    })
+
+    test("空配列から要素が追加された場合最初の要素に移動する", () => {
+        const handlePageChange = jest.fn()
+
+        const {rerender} = render(<Pagination paginateObjects={[]} handlePageChange={handlePageChange}/>);
+
+        // 初期化コール
+        expect(handlePageChange).toHaveBeenLastCalledWith(undefined);
+
+        rerender(<Pagination paginateObjects={[0]} handlePageChange={handlePageChange}/>)
+
+        // Propの変更によるコール
+        expect(handlePageChange).not.toHaveBeenLastCalledWith(undefined); // undefinedには決してならない
+        expect(handlePageChange).toHaveBeenLastCalledWith(0); // 現在の終端
     })
 
     test("初期地点は最初のrender時にしか影響しない", () => {
--- src/__tests__/TextParser.tsx
+++ src/__tests__/TextParser.tsx
@@ -5,13 +5,21 @@ import {fireEvent, render, screen} from "@testing-library/react";
 describe("text parsing", () => {
     test("空の文字列であれば空の配列を生成する", () => {
         expect(parse("")).toEqual([]);
+        expect(parse("")).toHaveLength(0);
     });
 
-    test("改行を含まない文字列はその文字列をdialogに持つObjectただひとつを要素とする配列を生成する", () => {
+    test("改行を含まない文字列はその文字列をdialogに持つObjectを要素とする配列を生成する", () => {
         expect(parse("foobarbaz")).toMatchObject([{dialog: "foobarbaz"}])
         expect(parse("ほげふが")).toMatchObject([{dialog: "ほげふが"}])
         expect(parse("foo<br>bar<br>baz")).toMatchObject([{dialog: "foo<br>bar<br>baz"}])
         expect(parse("foo\\nbar\\nbaz")).toMatchObject([{dialog: "foo\\nbar\\nbaz"}])
+    })
+
+    test("改行を含まない文字列はただ一つだけ要素を持つ配列を生成する", () => {
+        expect(parse("foobarbaz")).toHaveLength(1);
+        expect(parse("ほげふが")).toHaveLength(1);
+        expect(parse("foo<br>bar<br>baz")).toHaveLength(1);
+        expect(parse("foo\\nbar\\nbaz")).toHaveLength(1);
     })
 
     test("文字列の改行ごとに要素は分割される", () => {
@@ -20,14 +28,17 @@ describe("text parsing", () => {
             {dialog: "bar"},
             {dialog: "baz"}
         ])
+        expect(parse("foo\nbar\nbaz")).toHaveLength(3)
     })
 
     test("dialogが空文字列の要素は生成しない", () => {
         expect(parse("\n")).toEqual([]);
+        expect(parse("\n")).toHaveLength(0);
         expect(parse("foo\n\nbar")).toMatchObject([
             {dialog: "foo"},
             {dialog: "bar"}
         ]);
+        expect(parse("foo\n\nbar")).toHaveLength(2);
     })
 })
 
