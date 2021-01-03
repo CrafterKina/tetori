107114858b904a02143389ccc530f00824297b18
Kina <CrafterKina@users.noreply.github.com>
2021-01-03 16:05:20 +0900

    Add prop change test for Pagination

--- src/__tests__/Pagination.tsx
+++ src/__tests__/Pagination.tsx
@@ -46,5 +46,34 @@ describe("Pagination機能はArray要素を移動する機能を持つ", () => {
         expect(handlePageChange).toHaveBeenLastCalledWith(0);
     })
 
-    test.todo("要素数が変化した際にページを終端や始点に合わせる")
+    test("要素数が現地点より小さい場合にページを終端に合わせる", () => {
+        const handlePageChange = jest.fn()
+
+        const {rerender} = render(<Pagination paginateObjects={[0, 1, 2]} initialPosition={2}
+                                              handlePageChange={handlePageChange}/>);
+
+        // 初期化コール
+        expect(handlePageChange).toHaveBeenLastCalledWith(2);
+
+        rerender(<Pagination paginateObjects={[0, 1]} handlePageChange={handlePageChange}/>)
+
+        // Propの変更によるコール
+        expect(handlePageChange).not.toHaveBeenLastCalledWith(undefined); // undefinedには決してならない
+        expect(handlePageChange).toHaveBeenLastCalledWith(1); // 現在の終端
+    })
+
+    test("初期地点は最初のrender時にしか影響しない", () => {
+        const array = [0, 1, 2]
+        const handlePageChange = jest.fn()
+
+        const {rerender} = render(<Pagination paginateObjects={array} initialPosition={2}
+                                              handlePageChange={handlePageChange}/>);
+
+        // 初期化コール
+        expect(handlePageChange).toHaveBeenLastCalledWith(2);
+
+        rerender(<Pagination paginateObjects={array} initialPosition={0} handlePageChange={handlePageChange}/>)
+
+        expect(handlePageChange).toHaveBeenLastCalledWith(2); // 変化なし
+    })
 })
