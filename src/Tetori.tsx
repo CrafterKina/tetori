1dd76a52333d61ed35adc6b41103ee23a4b6c043
Kina <CrafterKina@users.noreply.github.com>
2021-01-05 11:56:31 +0900

    Remove unnecessary useEffect

--- src/Tetori.tsx
+++ src/Tetori.tsx
@@ -1,4 +1,4 @@
-import React, {Reducer, useEffect, useReducer} from "react";
+import React, {Reducer, useReducer} from "react";
 import {NavigableDialog} from "./NavigableDialog";
 
 export interface TetoriPage {
@@ -15,24 +15,24 @@ export type PosAction = { type: "next" } | { type: "previous" } | { type: "clamp
 
 export function Tetori(props: Props) {
     const [pos, dispatchPosMessage] = useReducer<Reducer<number, PosAction>>((state, action) => {
+            state = clamp(state, 0, props.contents.length - 1);
             switch (action.type) {
                 case "next":
                     return props.contents[state + 1] !== undefined ? state + 1 : state;
                 case "previous":
                     return props.contents[state - 1] !== undefined ? state - 1 : state;
-                case "clamp":
-                    return Math.max(0, Math.min(props.contents.length - 1, state));
                 default:
                     throw new Error();
             }
         },
         0);
 
-    useEffect(() => {
-        dispatchPosMessage({type: "clamp"})
-    }, [props.contents]);
-
     return (<div>
-        <NavigableDialog content={props.contents} pos={pos} dispatchPosMessage={dispatchPosMessage}/>
+        <NavigableDialog content={props.contents} pos={clamp(pos, 0, props.contents.length - 1)}
+                         dispatchPosMessage={dispatchPosMessage}/>
     </div>)
+}
+
+function clamp(value: number, min: number, max: number): number {
+    return Math.max(min, Math.min(max, value));
 }
