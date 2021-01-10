842bfc7932c860efee5bdb8d9876d4eb038e84f8
Kina <CrafterKina@users.noreply.github.com>
2021-01-10 15:40:07 +0900

    Simplify State transition

--- src/PlainTextParser.tsx
+++ src/PlainTextParser.tsx
@@ -1,62 +1,39 @@
 import {TetoriContents} from "./Tetori";
-import React, {Reducer, useEffect, useReducer} from "react";
-
+import React, {useState} from "react";
 
 type PlainTextParsing = { text: string, blanks: number | false, lines: number | false, allowEmpty: boolean };
 
 export function PlainTextParser(props: { onChange(params: { message: string, edit(): TetoriContents }): void }) {
     const {onChange} = props
-    const [state, dispatch] = useReducer<Reducer<PlainTextParsing,
-        { type: "text", text: string } |
-        { type: "blanks", num: number | false } |
-        { type: "lines", num: number | false } |
-        { type: "allowEmpty", tf: boolean }>>(
-        (state, action) => {
-            switch (action.type) {
-                case "text":
-                    return Object.assign({}, state, {text: action.text});
-                case "blanks":
-                    return Object.assign({}, state, {blanks: action.num});
-                case "lines":
-                    return Object.assign({}, state, {lines: action.num});
-                case "allowEmpty":
-                    return Object.assign({}, state, {allowEmpty: action.tf});
-                default:
-                    throw new Error();
-            }
-        }, {text: "", blanks: 1, lines: false, allowEmpty: true});
+    const [state, setState] = useState<PlainTextParsing>({text: "", blanks: 1, lines: false, allowEmpty: false});
 
-    useEffect(() => {
+    const update = (value: Partial<PlainTextParsing>) => setState(prevState => {
+        const state = Object.assign({}, prevState, value);
         onChange({
             message: `プレーンテキストから読み込み 空行数: ${state.blanks}, 行数: ${state.lines}, 空要素許容: ${state.allowEmpty}, テキスト: ${state.text.slice(0, 20)}`,
             edit(): TetoriContents {
                 return parsePlainText(state);
             }
         })
-    }, [onChange, state])
+        return state;
+    });
 
     return (
         <div>
             <div>
                 <label>
-                    <input type={"checkbox"} checked={state.blanks !== false} onChange={(e) => {
-                        dispatch({type: "blanks", num: e.target.checked ? 0 : false});
-                    }}/>
-                    <input type={"number"} disabled={state.blanks === false}
+                    <input type={"number"}
                            value={(state.blanks || 0).toString()}
                            onChange={(e) => {
-                               dispatch({type: "blanks", num: parseInt(e.target.value)});
+                               update({blanks: parseInt(e.target.value)});
                            }}/>
                     {"連続空行で段落とみなす"}
                 </label>
                 <label>
-                    <input type={"checkbox"} checked={state.lines !== false} onChange={(e) => {
-                        dispatch({type: "lines", num: e.target.checked ? 0 : false});
-                    }}/>
-                    <input type={"number"} disabled={state.lines === false}
+                    <input type={"number"}
                            value={(state.lines || 0).toString()}
                            onChange={(e) => {
-                               dispatch({type: "lines", num: parseInt(e.target.value)});
+                               update({lines: parseInt(e.target.value)});
                            }}/>
                     {"複数行を段落とみなす"}
                 </label>
@@ -64,14 +41,14 @@ export function PlainTextParser(props: { onChange(params: { message: string, edi
                     <input type={"checkbox"}
                            checked={state.allowEmpty}
                            onChange={(e) => {
-                               dispatch({type: "allowEmpty", tf: e.target.checked});
+                               update({allowEmpty: e.target.checked});
                            }}/>
                     {"空の要素を許容する"}
                 </label>
             </div>
             <textarea value={state.text}
-                      onChange={(event) => {
-                          dispatch({type: "text", text: event.target.value});
+                      onChange={(e) => {
+                          update({text: e.target.value});
                       }}/>
         </div>
     )
