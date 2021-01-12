2d06b64378b73f3c02ae2334c31b30f4dd72f9e6
Kina <CrafterKina@users.noreply.github.com>
2021-01-12 11:10:00 +0900

    Dialog collapse and scroll reset

--- src/NavigableDialog.tsx
+++ src/NavigableDialog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 type Props = {
@@ -9,14 +9,18 @@ type Props = {
 
 export function NavigableDialog(props: Props) {
     const {contents, pos, updatePos} = props;
+    const [collapse, setCollapse] = useState(true);
 
     return (<div className={"dialog"}>
-        <p className={"previous-dialog"} title={"Previous"}
-           onClick={() => {
+        <button onClick={() => setCollapse(!collapse)}>{`Prev ${collapse ? "▲" : "▼"}`}</button>
+        <p className={"previous-dialog"} title={"クリックで前へ"} hidden={collapse}
+           onClick={(e) => {
+               e.currentTarget.scrollTop = 0
                if (contents[pos - 1] !== undefined) updatePos(`ページ${pos - 1}`, {pos: pos - 1});
            }}>{contents[pos - 1] ?? "NA"}</p>
-        <p className={"next-dialog"} title={"Next"}
-           onClick={() => {
+        <p className={"next-dialog"} title={"クリックで次へ"}
+           onClick={(e) => {
+               e.currentTarget.scrollTop = 0
                if (contents[pos + 1] !== undefined) updatePos(`ページ${pos + 1}`, {pos: pos + 1});
            }}>{contents[pos] ?? "NA"}</p>
     </div>)
